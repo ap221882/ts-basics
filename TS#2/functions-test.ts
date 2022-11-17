454b4f5795ee5e3c4b63a7f36bbe93187f4a7fe1
ajay <ap221882@gmail.com>
2022-11-17 19:37:01 +0530

    without typing

---
+++ TS#2/functions-test.ts
@@ -0,0 +1,4 @@
+import addNumbers from "./functions";
+
+console.log(addNumbers(1, 2));
+console.log(addNumbers(1, "Jack"));
---
+++ TS#2/functions.ts
@@ -0,0 +1,5 @@
+function addNumbers(a, b) {
+  return a + b;
+}
+
+export default addNumbers;
