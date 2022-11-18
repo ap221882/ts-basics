92eb93bd2044da6b0250f8ea3300b921011cf63f
ajay <ap221882@gmail.com>
2022-11-18 15:02:48 +0530

    completed BS-TS#3

--- TS#2/functions-test.ts
+++ TS#2/functions-test.ts
@@ -1,4 +1,6 @@
-import addNumbers from "./functions";
+import addNumbers, { addStrings } from "./functions";
 
 console.log(addNumbers(1, 2));
-console.log(addNumbers(1, "Jack"));
+// console.log(addNumbers(1, "Jack"));
+
+console.log(addStrings("Ajay", "Pathak"));
--- TS#2/functions.ts
+++ TS#2/functions.ts
@@ -1,5 +1,28 @@
-function addNumbers(a, b) {
+function addNumbers(a: number, b: number): number {
   return a + b;
 }
 
+export function addStrings(a: string, b: string): string {
+  return a + b;
+}
+
+// Union Types
+const format = (title: string, param: string | number): string => {
+  return `${title} ${param}`;
+};
+
+//Void
+const print = (title: string, param: string | number): void => {
+  console.log(`${title} ${param}`);
+};
+
 export default addNumbers;
+
+//Promise
+export const fetchData = (url: string): Promise<string> =>
+  Promise.resolve(`Data from ${url}`);
+
+//rest
+function intro(salutation: string, ...names: string[]): string {
+  return `${salutation} ${names.join(" ")}`;
+}
