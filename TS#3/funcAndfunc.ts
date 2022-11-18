ee8e515997a76cca0b08d0f6b611f32b196f0a75
ajay <ap221882@gmail.com>
2022-11-18 15:13:34 +0530

    separated a type

--- TS#3/funcAndfunc.ts
+++ TS#3/funcAndfunc.ts
@@ -2,10 +2,11 @@ export function printToFile(text: string, callBack: () => void) {
   console.log(text);
   callBack();
 }
+type MutateFunction = (v: number) => number;
 
 export function arrayMutate(
   numbers: number[],
-  mutate: (v: number) => number
+  mutate: MutateFunction
 ): number[] {
   return numbers.map(mutate);
 }
